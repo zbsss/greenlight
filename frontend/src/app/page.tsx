095fc271dace7b9c81f4effe4c5c16d809d748ee
mkurleto <michal.kurleto@gmail.com>
2025-05-11 12:34:07 +0200

    fix lint

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -24,7 +24,6 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
         </ol>
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
